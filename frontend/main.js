7f65d642568b31e9eec421701847a861f7b5e8e5
masta <mastanilewicz@gmail.com>
2022-08-18 09:37:49 +0100

    loader when fetching data

--- frontend/main.js
+++ frontend/main.js
@@ -2,26 +2,25 @@ import { addRectOnGrid, getData } from "./data.js"
 const grid = document.querySelector('#game-board')
 const userInput = document.querySelector('#find-user')
 const form = document.querySelector('#form')
+const loader = document.querySelector('.loader')
 
-const DEFAULT_USER = 'dieBijacz'
-
-// input user
-form.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  if (userInput.value === '') {
-    setupDefaultUser()
-  } else {
-    grid.innerHTML = ''
-    const data = await getData(userInput.value)
-    addRectOnGrid(data)
-  }
-})
-
-// Default user
-async function setupDefaultUser() {
+async function fetchHandler(user = 'dieBijacz') {
   grid.innerHTML = ''
-  const data = await getData(DEFAULT_USER)
+  loader.classList.add('show')
+  grid.style.border = 'none'
+  const data = await getData(user)
   addRectOnGrid(data)
+  if (data) {
+    loader.classList.remove('show')
+    grid.style.border = '1px solid #D2D2D2'
+  }
 }
 
-setupDefaultUser()
+// user input
+form.addEventListener('submit', async (e) => {
+  if (userInput.value === '') return 'dieBijacz'
+  e.preventDefault()
+  fetchHandler(userInput.value)
+})
+
+fetchHandler()
